2987b1103a74cf380a0b9582417da3d5fc9cd7e3
Ivan Mozheev <mau0hezzz@hotmail.com>
2026-01-16 13:22:05 +0300

    listing final

--- vitejs/pages.config.js
+++ vitejs/pages.config.js
@@ -4,6 +4,7 @@ const pages = [
   {name: 'index', path: resolve(__dirname, '../index.html')},
   {name: 'home', path: resolve(__dirname, '../pages/home.html')},
   {name: 'listing', path: resolve(__dirname, '../pages/listing.html')},
+  {name: 'product', path: resolve(__dirname, '../pages/product.html')},
 ];
 
 export default pages
