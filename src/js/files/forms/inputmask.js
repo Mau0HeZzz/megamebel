788b8b754e08e3f42766b39940ce5e0b71123907
Ivan Mozheev <mau0hezzz@hotmail.com>
2026-01-19 11:47:44 +0300

    comparsion final

--- src/js/files/forms/inputmask.js
+++ src/js/files/forms/inputmask.js
@@ -6,7 +6,7 @@ import { mhzModules } from "../modules.js";
 // Подключение модуля
 import "inputmask/dist/inputmask.min.js";
 
-const inputMasks = document.querySelectorAll('input');
+const inputMasks = document.querySelectorAll('[data-inputmask]');
 if (inputMasks.length) {
 	mhzModules.inputmask = Inputmask().mask(inputMasks);
 }
